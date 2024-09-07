3a836d40043cd4463129b1a6d60e9e20cd7b0969
AndresChCy <aichaparrom@gmail.com>
2024-09-07 01:52:31 -0400

    Se arregla problema de parametros y se hace un pequeño test

--- leePipes.c
+++ leePipes.c
@@ -4,58 +4,73 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-/*
-*Funcion pipes para interpretar Pipes en el comando
-cantPipes: Cantidad de pipes en el comando
-char**** args: Matriz de strings para separar cada palabra de un comando y separar cada comando por pipes de la entrada
-*/
 
-void pipes(int cantPipes, const char***args[]){
-  int status;
-  int i;
-  //Arreglo para usar el pipe
-  int pipes[cantPipes*2];
-  // Se transforma el arreglo  en pipes
-  for (int i = 0; i < cantPipes; i++) {
-    pipe(pipes+i*2);
-  }
-  //Primer proceso para servir de escritura para el siguiente proceso
-  if(fork()== 0){
-    dup2(pipes[1], 1);
-    //Se cierran los demas espacios del pipe ya que no se usan 
-    for (int i = 0; i < cantPipes*2; i++){
+/*
+ * Función pipes para interpretar Pipes en el comando
+ * cantPipes: Cantidad de pipes en el comando
+ * char**** args: Matriz de strings para separar cada palabra de un comando y separar cada comando por pipes de la entrada
+ */
+
+void pipes(int cantPipes, const char **args[]) {
+    printf("hola\n");
+    int status;
+    int i;
+    // Arreglo para usar el pipe
+    int pipes[cantPipes * 2];
+    // Se transforma el arreglo en pipes
+    for (i = 0; i < cantPipes; i++) {
+        pipe(pipes + i * 2);
+    }
+    // Primer proceso para servir de escritura para el siguiente proceso
+    if (fork() == 0) {
+        printf("a1\n");
+        dup2(pipes[1], 1);
+        // Se cierran los demás espacios del pipe ya que no se usan
+        for (i = 0; i < cantPipes * 2; i++) {
+            close(pipes[i]);
+        }
+        execvp(args[0][0], (char *const *)args[0]);
+        printf("err1\n");
+    }
+    // Un for para los demás procesos que serán lecturas y escrituras
+    for (i = 0; i < cantPipes - 1; i++) {
+        if (fork() == 0) {
+            printf("a2\n");
+            dup2(pipes[i * 2], 0);
+            dup2(pipes[(i * 2) + 3], 1);
+            for (int j = 0; j < cantPipes * 2; j++) {
+                close(pipes[j]);
+            }
+            execvp(args[i+1][0],(char *const *)args[i+1]);
+            printf("err %i\n",i );
+        }
+    }
+    // Fork para el último proceso del comando y que lea la última escritura
+    if (fork() == 0) {
+        printf("a3\n");
+        dup2(pipes[cantPipes * 2 - 2], 0);
+        for (i = 0; i < cantPipes * 2; i++) {
+            close(pipes[i]);
+        }
+        execvp(args[cantPipes][0],(char *const *)args[cantPipes]);
+        printf("err final\n");
+    }
+
+    // Padre solo espera por hijos y cierra todos los pipes porque no los usa
+    for (i = 0; i < cantPipes * 2; i++) {
         close(pipes[i]);
     }
-    execvp(***args, **args);
-  }
-  //Un for para los demas procesos que seran lecturas y escrituras
-  for (int i = 0; i < cantPipes; i++){
-    if (fork() == 0){    
-      dup2(pipes[i*2], 0);
-      dup2(pipes[(i*2)+3], 1);
-      for (int i = 0; i < cantPipes*2; i++){
-          close(pipes[i]);
-      }
-
-      execvp(***(args +i+1), **(args+i+1));
+    for (i = 0; i < cantPipes + 1; i++) {
+        wait(&status);
     }
-  }
-  //Fork para el ultimo proceso del comando y que lea la ultima escritura
-  if (fork() == 0)   {
+}
 
-        dup2(pipes[cantPipes*2 -1], 0);
-        for (int i = 0; i < cantPipes*2; i++){
-          close(pipes[i]);
-      }
-        execvp(***(args+cantPipes+1), **(args+cantPipes+1));
-      }
-
-      
-  // padre solo espera por hijos y cierra todos los pipes porque no los usa
-  
-  for (int i = 0; i < cantPipes*2; i++){
-          close(pipes[i]);
-  }
-  for (i = 0; i < cantPipes+1; i++)
-    wait(&status);
+int main(){
+    char *cat_args[] = {"cat", "test.txt", NULL};
+    char *grep_args[] = {"grep", "hola", NULL};
+    char *wc_args[] = {"wc",NULL};
+    char** com[] = {cat_args,grep_args,wc_args} ;
+    pipes(2,(const char***)com);
+    printf("termine\n");
+    return 0;
 }
